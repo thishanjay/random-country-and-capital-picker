fd6dc13ec2853f3ae3f4d6452617cf95bc6ba6b3
Thishan-Jay <thishanjayasundara11@gmail.com>
2025-12-02 00:09:12 +0530

    css improvement

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 import { useState } from "react";
 
 type CountryResponse = {
   country: string;
   capital: string;
   flag?: string;
-}
+};
 
 export default function Home() {
   const [data, setData] = useState<CountryResponse | null>(null);
@@ -17,30 +17,62 @@ export default function Home() {
   }
 
   return (
-    <div className="p-30">
-      <h1 className="text-7xl font-mono p-8">Random Country & Capital Picker</h1>
-      <div className="flex gap-60">
+    <div className="min-h-screen flex flex-col items-center justify-center p-8">
+      <h1 className="text-5xl md:text-6xl font-mono font-bold mb-12 text-center p-8">
+        Random Country & Capital Picker
+      </h1>
+
+      <div className="flex flex-col md:flex-row justify-between items-center gap-12">
         {/* Country */}
-        <div className="flex items-center gap-4">
-          <p className="p-8">Country: {data?.country || "---"}</p>
-
+        <div className="flex-1 flex flex-col items-center md:items-start">
+          <p className="text-2xl font-semibold">Country: </p>
+          <p className="text-3xl md:text-4xl font-bold">{data?.country || "---"}</p>
           {data?.country && (
-            <button onClick={() => window.open(`https://en.wikipedia.org/wiki/${encodeURIComponent(data.country)}`, "_blank")} className="text-blue-600 underline hover:cursor-pointer">Wikipedia</button>
+            <button
+              onClick={() =>
+                window.open(
+                  `https://en.wikipedia.org/wiki/${encodeURIComponent(
+                    data.country
+                  )}`,
+                  "_blank"
+                )
+              }
+              className="text-blue-600 underline hover:cursor-pointer"
+            >
+              Wikipedia
+            </button>
           )}
-
         </div>
+
         {/* Capital */}
-        <div className="flex items-center gap-4">
-          <p className="p-8">Capital: {data?.capital || "---"}</p>
+        <div className="flex-1 flex flex-col items-center md:items-start">
+          <p className="text-2xl font-semibold">Capital: </p>
+          <p className="text-3xl md:text-4xl font-bold">{data?.capital || "---"}</p>
 
           {data?.capital && (
-            <button onClick={() => window.open(`https://en.wikipedia.org/wiki/${encodeURIComponent(data.capital)}`, "_blank")} className="text-blue-600 underline hover:cursor-pointer">Wikipedia</button>
+            <button
+              onClick={() =>
+                window.open(
+                  `https://en.wikipedia.org/wiki/${encodeURIComponent(
+                    data.capital
+                  )}`,
+                  "_blank"
+                )
+              }
+              className="text-blue-600 underline hover:cursor-pointer"
+            >
+              Wikipedia
+            </button>
           )}
-
         </div>
       </div>
       <div>
-        <button className="m-8 p-4 bg-cyan-600 hover:cursor-pointer rounded-2xl hover:bg-cyan-700" onClick={fetchCountry}>Generate</button>
+        <button
+          className="mt-12 px-8 py-4 bg-cyan-600 hover:bg-cyan-800 font-semibold rounded-2xl shadow-lg transform transition hover:scale-105"
+          onClick={fetchCountry}
+        >
+          Generate random country
+        </button>
       </div>
     </div>
   );
